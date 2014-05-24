6a3e8e37640bad67ca258973ef2dce733871ca5d
yiifans <yiifans@sina.com>
2014-05-24 12:37:14 +0800

    fix bug :Duplicate  column modify_time

--- data/sql/yiiforum_2014052401.sql
+++ data/sql/yiiforum_2014052401.sql
@@ -1,5 +1,5 @@
-ALTER TABLE  `board` ADD  `modify_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ;
-ALTER TABLE  `board` ADD  `user_id` INT NULL DEFAULT  '0' AFTER  `id` ,
+ALTER TABLE  `board` CHANGE  `modify_time`  `modify_time` TIMESTAMP NULL DEFAULT NULL ;
+ALTER TABLE  `board` ADD  `user_id` INT NULL DEFAULT  '0' AFTER  `modify_time` ,
 ADD  `user_name` VARCHAR( 32 ) NULL DEFAULT NULL AFTER  `user_id` ,
 ADD  `thread_id` INT NULL DEFAULT  '0' AFTER  `user_name` ,
 ADD  `thread_title` VARCHAR( 256 ) NULL DEFAULT NULL AFTER  `thread_id` ;
